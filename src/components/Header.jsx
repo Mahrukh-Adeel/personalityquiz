34a91716856ee9a15e1f866e4664857743a5f751
Mahrukh Adeel <150025532+Mahrukh-Adeel@users.noreply.github.com>
2024-09-10 18:24:27 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header(){
     return(
         <>
             <h1 className="text-2xl font-bold pb-2">Find out what element you are</h1>
-            <p className="">(With random art works for you)</p>
+            <p className="">(With random artwork for you)</p>
             <div className="my-2">
                 <Link className="mr-2 p-1 rounded hover:bg-gray-300 box-border font-semibold focus:ring-1 focus:ring-black" to="/">Home</Link>
                 <Link className="mr-2 p-1 rounded hover:bg-gray-300 box-border font-semibold focus:ring-1 focus:ring-black" to="/Quiz">Quiz</Link>
